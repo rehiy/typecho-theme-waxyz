34acc798dc179743d11d733a2414cbc49e7f5e6f
若海 <wang@rehiy.com>
2022-12-04 00:07:50 +0800

    增强代码可读性

--- js/waxyz.js
+++ js/waxyz.js
@@ -47,8 +47,8 @@ $(document).ready(function () {
 });
 
 // 鼠标特效
-var a_idx = 0;
 jQuery(document).ready(function ($) {
+    var a_idx = 0;
     $("body").click(function (e) {
         var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
         var $i = $("<span></span>").text(a[a_idx]);
@@ -75,77 +75,64 @@ jQuery(document).ready(function ($) {
     });
 });
 
-// 天空之城
+// 菜单音乐 - 天空之城
 jQuery(document).ready(function () {
-    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    var AudioContext = window.AudioContext || window.webkitAudioContext;
 
     if (!AudioContext) {
         return;
     }
 
-    const sheet = '880 987 1046 987 1046 1318 987 659 659 880 784 880 1046 784 659 659 698 659 698 1046 659 1046 1046 1046 987 698 698 987 987 880 987 1046 987 1046 1318 987 659 659 880 784 880 1046 784 659 698 1046 987 1046 1174 1174 1174 1046 1046 880 987 784 880 1046 1174 1318 1174 1318 1567 1046 987 1046 1318 1318 1174 784 784 880 1046 987 1174 1046 784 784 1396 1318 1174 659 1318 1046 1318 1760 1567 1567 1318 1174 1046 1046 1174 1046 1174 1567 1318 1318 1760 1567 1318 1174 1046 1046 1174 1046 1174 987 880 880 987 880'.split(' ');
+    var i = 0, ctx, dom;
 
-    let ctx,
-        i = 0,
-        o = 1,
-        dom,
-        a = '♪ ♩ ♫ ♬ ♭ € § ¶ ♯'.split(' '),
-        selects = document.querySelectorAll('.menu li')
-        ;
+    var notes = '♪ ♩ ♫ ♬ ♭ € § ¶ ♯'.split(' ');
+    var sheet = '880 987 1046 987 1046 1318 987 659 659 880 784 880 1046 784 659 659 698 659 698 1046 659 1046 1046 1046 987 698 698 987 987 880 987 1046 987 1046 1318 987 659 659 880 784 880 1046 784 659 698 1046 987 1046 1174 1174 1174 1046 1046 880 987 784 880 1046 1174 1318 1174 1318 1567 1046 987 1046 1318 1318 1174 784 784 880 1046 987 1174 1046 784 784 1396 1318 1174 659 1318 1046 1318 1760 1567 1567 1318 1174 1046 1046 1174 1046 1174 1567 1318 1318 1760 1567 1318 1174 1046 1046 1174 1046 1174 987 880 880 987 880'.split(' ');
 
-    selects.forEach((select) => {
-        select.addEventListener('mouseenter', (e) => {
+    document.querySelectorAll('.menu li').forEach((el) => {
+        el.addEventListener('mouseenter', (e) => {
+            e.stopPropagation();
+
             if (dom) {
                 return;
             }
-
-            let r = sheet[i];
-            if (!r) {
-                i = 0;
-                r = sheet[i];
-            }
-            i += o;
 
             if (!ctx) {
                 ctx = new AudioContext;
             }
 
-            const c = ctx.createOscillator(), l = ctx.createGain(), mainGain = ctx.createGain();
+            sheet[i] || (i = 0);
+
+            var c = ctx.createOscillator(),
+                l = ctx.createGain(),
+                m = ctx.createGain();
             c.connect(l);
-            l.connect(mainGain);
-            mainGain.connect(ctx.destination);
-            mainGain.gain.setValueAtTime(1, ctx.currentTime);
+            l.connect(m);
+            m.connect(ctx.destination);
+            m.gain.setValueAtTime(1, ctx.currentTime);
             c.type = 'sine';
-            c.frequency.value = r;
+            c.frequency.value = sheet[i];
             l.gain.setValueAtTime(0, ctx.currentTime);
             l.gain.linearRampToValueAtTime(1, ctx.currentTime + .01);
             c.start(ctx.currentTime);
             l.gain.exponentialRampToValueAtTime(.001, ctx.currentTime + 1);
             c.stop(ctx.currentTime + 1);
 
-            const d = Math.round(7 * Math.random());
-            const h = e.pageX;
-            const p = e.pageY - 5;
+            var x = e.pageX,
+                y = e.pageY - 5,
+                d = Math.round(7 * Math.random());
             dom = document.createElement('b');
-            dom.textContent = a[d];
+            dom.textContent = notes[d];
             dom.style.zIndex = '99999';
-            dom.style.top = p - 100 + 'px';
-            dom.style.left = h + 'px';
+            dom.style.top = y - 100 + 'px';
+            dom.style.left = x + 'px';
             dom.style.position = 'absolute';
             dom.style.color = '#FF6EB4';
             document.body.appendChild(dom);
-            dom.animate([
-                { top: p + 'px' },
-                { opacity: 0 }
-            ], {
-                duration: 500
-            });
-            setTimeout(() => {
-                dom.remove();
-                dom = null;
-            }, 500);
+            dom.animate([{ top: y + 'px' }, { opacity: 0 }], { duration: 500 });
 
-            e.stopPropagation();
+            setTimeout(() => { dom.remove(); dom = null; }, 500);
+
+            i++;
         });
     });
 
