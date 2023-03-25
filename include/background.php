88a0a068747b7b9f35bdc627791afd90c703c72f
若海 <wang@rehiy.com>
2023-03-25 21:40:33 +0800

    背景视频允许切换为画中画

--- include/background.php
+++ include/background.php
@@ -14,16 +14,83 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 $media_type = pathinfo($media_url, PATHINFO_EXTENSION);
 ?>
 
-<style type="text/css">
-    .content-wrap {
-        opacity: 0.9;
-    }
-</style>
+<?php if (preg_match('/jpeg|jpg|png|gif/i', $media_type)) : ?>
 
-<div style="position: fixed; top: 0; z-index: -1000;">
-    <?php if (preg_match('/jpeg|jpg|png|gif/i', $media_type)) : ?>
+    <div class="background-media">
         <img src="<?php echo $media_url; ?>" width="auto" />
-    <?php elseif (preg_match('/mp4/i', $media_type)) : ?>
+    </div>
+
+    <style type="text/css">
+        .content-wrap {
+            opacity: 0.9;
+        }
+
+        .background-media {
+            position: fixed;
+            top: 0;
+            z-index: -1000;
+        }
+    </style>
+
+<?php elseif (preg_match('/mp4/i', $media_type)) : ?>
+
+    <a class="background-ctrl">
+        <i class="glyphicon glyphicon-log-in"></i>
+    </a>
+    <div class="background-media">
         <video loop muted autoplay width="auto" type="video/mp4" src="<?php echo $media_url; ?>"></video>
-    <?php endif; ?>
-</div>
+    </div>
+
+    <style type="text/css">
+        .content-wrap {
+            opacity: 0.9;
+        }
+
+        .background-media {
+            position: fixed;
+            top: 0;
+            z-index: -1000;
+        }
+
+        .background-ctrl {
+            position: fixed;
+            right: 25px;
+            bottom: 80px;
+            background: rgba(244, 100, 95, 0.6);
+            color: #ffffff;
+            text-align: center;
+            border-radius: 6px;
+            z-index: 1;
+        }
+
+        .background-ctrl:hover {
+            background: #f4645f;
+            color: #ffffff;
+        }
+
+        .background-ctrl i {
+            width: 45px;
+            height: 45px;
+            line-height: 45px;
+        }
+    </style>
+
+    <script type="text/javascript">
+        var pictureInPicture = false
+        var $btn = document.querySelector('.background-ctrl')
+        var $mda = document.querySelector('.background-media')
+        var $mp4 = document.querySelector('.background-media video')
+        $mp4.addEventListener('enterpictureinpicture', (event) => {
+            $mda.style.display = 'none'
+            pictureInPicture = true
+        });
+        $mp4.addEventListener('leavepictureinpicture', (event) => {
+            $mda.style.display = 'block'
+            pictureInPicture = false
+        });
+        $btn.addEventListener('click', function() {
+            pictureInPicture ? document.exitPictureInPicture() : $mp4.requestPictureInPicture()
+        })
+    </script>
+
+<?php endif; ?>
