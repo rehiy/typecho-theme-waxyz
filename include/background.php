0ae8968f91e3347cf1d867ac566a2dd00f655df3
若海 <wang@rehiy.com>
2023-04-03 11:34:30 +0800

    feat: 新增播放声音按钮

--- include/background.php
+++ include/background.php
@@ -26,9 +26,14 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
 <?php elseif (preg_match('/video|app/i', $media_type)) : ?>
 
-    <a class="background-ctrl">
-        <i class="glyphicon glyphicon glyphicon-film"></i>
-    </a>
+    <div class="background-ctrls">
+        <a class="btn-volume">
+            <i class="glyphicon glyphicon-volume-up"></i>
+        </a>
+        <a class="btn-display">
+            <i class="glyphicon glyphicon-eye-open"></i>
+        </a>
+    </div>
 
     <div class="background-media">
         <video id="bg-media" class="video-js" style="width:100%;height:100%">
@@ -39,21 +44,27 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
     <script type="text/javascript">
         var player = videojs('bg-media', {
             controls: true,
-            autoplay: "any",
+            autoplay: 'any',
             preload: 'auto',
             loop: true,
         });
-        var $ctl = document.querySelector('.background-ctrl')
-        var $bgm = document.querySelector('.background-media')
         player.on('loadeddata', (event) => {
-            document.querySelector('.main-navigation').style.opacity = 0.9
-            document.querySelector('.content-wrap').style.opacity = 0.8
-            $ctl.style.display = 'block'
-            $bgm.style.opacity = 1
+            $('.content-wrap').animate({
+                'opacity': 0.8
+            }, 1500)
+            $('.main-navigation').animate({
+                'opacity': 0.9
+            }, 1500)
+            $('.background-media, .background-ctrls').animate({
+                'opacity': 1
+            }, 1500)
+            $('.background-ctrls .btn-volume').click(function() {
+                player.muted() ? player.muted(false) : player.muted(true)
+            })
+            $('.background-ctrls .btn-display').click(function() {
+                $('.background-media').toggleClass('small')
+            })
         });
-        $ctl.addEventListener('click', function() {
-            $bgm.classList.contains('small') ? $bgm.classList.remove('small') : $bgm.classList.add('small')
-        })
     </script>
 
 <?php endif; ?>
